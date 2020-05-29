224e7066893f3dd7f1b4f89680cbe0d3a1812419
Mac <mac@MacBook-Pro-mac.local>
2020-05-29 15:48:31 +0300

    Vol-1

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+let status = "JS - OK!";
+// =================
+// @@include('frames/globalFunctions.js')
+// ===============
+$(document).ready(function() {
+	if (!$('body')) {console.log('jQuery Error')}
+	// console.log('document ready')
+	// console.log(dropDown)	
+	// =================Include Modules==============================
+	// console.log(test);
+
+	// if (document.documentElement.clientWidth > 991 ) {}
+
+	
+
+});
+
+window.onload = function() {
+	// console.log('window load')
+}
