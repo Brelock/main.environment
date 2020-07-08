077b45edbacac3148058f1285a986517e28a370c
Alex Bob <bboybrelock@gmail.com>
2020-07-08 11:50:01 +0300

    edits js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,57 @@
 let status = "JS - OK!";
+
+let site = null;
 // =================
 // @@include('frames/globalFunctions.js')
 // ===============
-$(document).ready(function() {
-	if (!$('body')) {console.log('jQuery Error')}
+document.addEventListener("DOMContentLoaded", function(event) { 
+    if (  !document.getElementsByTagName("body")) {console.log('js error')} 
+    
 	// console.log('document ready')
-	// console.log(dropDown)	
+	// if (document.documentElement.clientWidth > 991 ) {}
+	
 	// =================Include Modules==============================
 
 	  /*@@include('frames/modal.js')*/
-	
+	  
+	  
 
-	// if (document.documentElement.clientWidth > 991 ) {}
+	site = (function() {
+
+		const win = window,
+			  dom = document,
+			  body = document.body,
+			  app = {}
+			  
+			  name = document.getElementsByClassName("mainWrapper")
+
+		
+	
+		const fn = {
+			Function() {
+				console.log("AlexBob it's ok");
+				console.log("site earned");
+				
+			},
+
+			
+			domReady() {
+				if (name.length) {
+					fn.Function();
+				}
+			}
+		}
+		
+		dom.addEventListener("DOMContentLoaded", fn.domReady()) ;
+	
+		return fn;
+	
+	  })();
 
 	
 
 });
 
 window.onload = function() {
-	// console.log('window load')
+	console.log('Hi man! this window load')
 }
